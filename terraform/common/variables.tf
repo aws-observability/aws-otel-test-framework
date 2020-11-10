d6d6745ba590e792103a607c73f2dc7a20efe4b1
xiami <xiami@amazon.com>
2020-11-10 11:29:21 -0800

    fix some naming issue

--- terraform/common/variables.tf
+++ terraform/common/variables.tf
@@ -26,11 +26,11 @@ variable "aoc_iam_role" {
 }
 
 variable "otel_service_namespace" {
-  default = "MinSoaking"
+  default = "aws-otel"
 }
 
 variable "otel_service_name" {
-  default = "Terraform"
+  default = "aws-otel-soaking"
 }
 
 variable "aoc_vpc_name" {
