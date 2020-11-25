934ddb921a140ad36e82f06ce1fb0b7063c022d1
Shaocheng Wang <shaochew@amazon.com>
2020-11-24 16:52:37 -0800

    remove unnecessary variables from canary config

--- terraform/canary/variables.tf
+++ terraform/canary/variables.tf
@@ -28,24 +28,6 @@ variable "soaking_compose_file" {
   default = ""
 }
 
-######################
-# Soaking related
-######################
-# data type will be emitted. Possible values: metric or trace
-variable "soaking_data_mode" {
-  default = "metric"
-}
-
-# data points were emitted per second
-variable "soaking_data_rate" {
-  default = 100
-}
-
-# data model type. possible values: otlp, xray, etc
-variable "soaking_data_type" {
-  default = "otlp"
-}
-
 variable "skip_validation" {
   default = false
 }
