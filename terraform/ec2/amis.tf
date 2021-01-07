3c3c64e0dfd099361eb51bf862fdebc6acab8e56
Ying <wangmyin@amazon.com>
2021-01-06 16:02:34 -0800

    Expand ec2 test for more amis and arm (#210)
    * expand ec2 test to multi amis
    
    * add arm amis for testing

--- terraform/ec2/amis.tf
+++ terraform/ec2/amis.tf
@@ -15,7 +15,7 @@
 
 variable "ami_family" {
   default = {
-    ubuntu = {
+    debian = {
       login_user = "ubuntu"
       install_package = "aws-otel-collector.deb"
       instance_type = "t2.micro"
@@ -26,19 +26,19 @@ variable "ami_family" {
       connection_type = "ssh"
       user_data = ""
     }
-    amazon_linux = {
+    linux = {
       login_user = "ec2-user"
       install_package = "aws-otel-collector.rpm"
       instance_type = "t2.micro"
       otconfig_destination = "/tmp/ot-default.yml"
-      download_command_pattern = "wget %s"
+      download_command_pattern = "curl %s --output aws-otel-collector.rpm"
       install_command = "sudo rpm -Uvh aws-otel-collector.rpm"
       start_command = "sudo /opt/aws/aws-otel-collector/bin/aws-otel-collector-ctl -c /tmp/ot-default.yml -a start"
       connection_type = "ssh"
       user_data = ""
     }
     windows = {
-      login_user = "Administrator"
+
       install_package = "aws-otel-collector.msi"
       instance_type = "t3.medium"
       otconfig_destination = "C:\\ot-default.yml"
@@ -65,26 +65,185 @@ EOF
   }
 }
 
+########################################
+##  define the amis to test collector ##
+##
+## os_family, which will be used to construct the downloading url of collector on s3
+## ami_search_pattern, which will be used to search the amis from aws
+## ami_owner, could be "amazon", "aws-marketplace", or a dedicated account number, for example, redhat amis are distributed by this account 309956199498(Redhat Inc)
+## family, could be "linux", "debian", "windows"
+## arch, which will be used to construct the downloading url of collector on s3
+## login_user, which will be taken first, if null then the take from family.
+########################################
 variable "amis" {
   default = {
     ubuntu16 = {
+      os_family = "ubuntu"
       ami_search_pattern = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-amd64-server-*"
       ami_owner = "099720109477"
-      family = "ubuntu"
+      family = "debian"
       arch = "amd64"
+      login_user = "ubuntu"
+    }
+    ubuntu18 = {
+      os_family = "ubuntu"
+      ami_search_pattern = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server*"
+      ami_owner = "099720109477"
+      family = "debian"
+      arch = "amd64"
+      login_user = "ubuntu"
+    }
+    debian10 = {
+      os_family = "debian"
+      ami_search_pattern = "debian-10-amd64*"
+      ami_owner = "679593333241"
+      family = "debian"
+      arch = "amd64"
+      login_user = "admin"
+    }
+    debian9 = {
+      os_family = "debian"
+      ami_search_pattern = "debian-stretch-hvm-x86_64-gp2*"
+      ami_owner = "679593333241"
+      family = "debian"
+      arch = "amd64"
+      login_user = "admin"
     }
     amazonlinux2 = {
+      os_family = "amazon_linux"
       ami_search_pattern = "amzn2-ami-hvm*"
       ami_owner = "amazon"
       family = "amazon_linux"
       arch = "amd64"
+      login_user = "ec2-user"
     }
     windows2019 = {
+      os_family = "windows"
       ami_search_pattern = "Windows_Server-2019-English-Full-Base-*"
       ami_owner = "amazon"
       family = "windows"
       arch = "amd64"
+      login_user = "Administrator"
     }
+    amazonlinux = {
+      os_family = "amazon_linux"
+      ami_search_pattern = "amzn-ami-hvm*"
+      ami_owner = "amazon"
+      family = "amazon_linux"
+      arch = "amd64"
+      login_user = "ec2-user"
+    }
+    suse15 = {
+      os_family = "suse"
+      ami_search_pattern = "suse-sles-15*"
+      family = "linux"
+      login_user = "ec2-user"
+      arch = "amd64"
+      ami_owner = "amazon"
+    }
+    suse12 = {
+      os_family = "suse"
+      ami_search_pattern = "suse-sles-12*"
+      family = "linux"
+      login_user = "ec2-user"
+      arch = "amd64"
+      ami_owner = "amazon"
+    }
+    redhat8 = {
+      os_family = "redhat"
+      ami_search_pattern = "RHEL-8.0.0_HVM*"
+      family = "linux"
+      ami_owner = "309956199498"
+      arch = "amd64"
+    }
+    redhat7 = {
+      os_family = "redhat"
+      ami_search_pattern = "RHEL-7.7_HVM*"
+      family = "linux"
+      ami_owner = "309956199498"
+      arch = "amd64"
+    }
+    centos7 = {
+      login_user = "centos"
+      os_family = "centos"
+      ami_search_pattern = "CentOS Linux 7 x86_64*"
+      family = "linux"
+      ami_owner = "679593333241"
+      arch = "amd64"
+    }
+    centos6 = {
+      login_user = "centos"
+      os_family = "centos"
+      ami_search_pattern = "CentOS Linux 6 x86_64 HVM EBS 2002*"
+      family = "linux"
+      ami_owner = "679593333241"
+      arch = "amd64"
+      user_data = <<EOF
+#! /bin/bash
+sudo iptables -I INPUT -p tcp -m tcp --dport 55680 -j ACCEPT
+sudo iptables -I INPUT -p tcp -m tcp --dport 55690 -j ACCEPT
+sudo service iptables save
+EOF
+    }
+
+
+    # arm amis
+    arm_amazonlinux2 = {
+      os_family = "amazon_linux"
+      ami_search_pattern = "amzn2-ami-hvm-2.0.20200722.0-arm64*"
+      family = "linux"
+      ami_owner = "amazon"
+      arch = "arm64"
+      instance_type = "t4g.nano"
+    }
+
+    arm_suse15 = {
+      os_family = "suse"
+      ami_search_pattern = "suse-sles-15-sp2-v20200721-hvm-ssd-arm64*"
+      family = "linux"
+      ami_owner = "amazon"
+      arch = "arm64"
+      instance_type = "t4g.nano"
+    }
+
+    arm_redhat8 = {
+      os_family = "redhat"
+      ami_search_pattern = "RHEL-8.0.0_HVM-20190426-arm64*"
+      family = "linux"
+      ami_owner = "309956199498"
+      arch = "arm64"
+      instance_type = "t4g.micro"
+    }
+
+    arm_redhat7 = {
+      os_family = "redhat"
+      ami_search_pattern = "RHEL-7.6_HVM_GA-20181122-arm64*"
+      family = "linux"
+      ami_owner = "309956199498"
+      arch = "arm64"
+      instance_type = "t4g.micro"
+    }
+
+    arm_ubuntu18 = {
+      os_family = "ubuntu"
+      ami_search_pattern = "ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-arm64*"
+      family = "debian"
+      ami_owner = "099720109477"
+      arch = "arm64"
+      instance_type = "t4g.nano"
+    }
+
+    arm_ubuntu16 = {
+      os_family = "ubuntu"
+      ami_search_pattern = "ubuntu/images/hvm-ssd/ubuntu-xenial-16.04-arm64*"
+      family = "debian"
+      ami_owner = "099720109477"
+      arch = "arm64"
+      instance_type = "t4g.nano"
+    }
+
+
+
   }
 }
 
