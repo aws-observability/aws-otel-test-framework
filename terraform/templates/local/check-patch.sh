46252d09628c27c5779121f303826780b682c359
Ying <wangmyin@amazon.com>
2021-01-22 10:25:30 -0800

    Update check-patch.sh

--- terraform/templates/local/check-patch.sh
+++ terraform/templates/local/check-patch.sh
@@ -5,7 +5,7 @@ set -e
 instance_id=$1
 
 n=0
-until [ "$n" -ge 20 ]
+until [ "$n" -ge 5 ]
 do
   aws ssm describe-instance-associations-status --instance-id "$instance_id" | grep "AWS-RunPatchBaseline" -A10 | grep "Success" && echo "patch finished" && break
   echo "waiting for patch, sleep 1 minute"
