57b46ecd0f9f7049a26d99daff84094699caccfd
Ying <wangmyin@amazon.com>
2021-01-29 17:48:22 -0800

    update vpc to large one (#222)
    * Update setup.tf
    
    * Update outputs.tf

--- terraform/setup/setup.tf
+++ terraform/setup/setup.tf
@@ -78,8 +78,8 @@ module "vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  private_subnets = ["10.0.1.0/16", "10.0.2.0/16", "10.0.3.0/16"]
+  public_subnets  = ["10.0.101.0/16", "10.0.102.0/16", "10.0.103.0/16"]
 
   enable_nat_gateway = true
   enable_vpn_gateway = true
