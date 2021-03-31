f9756f985782b0e3ea1ea8b44ff3e9ec77c3f1f6
Pinglei Guo <pingleig@amazon.com>
2021-03-31 14:46:05 -0700

    patch: Use cobra to include sub commands

--- hack/aotutil/main.go
+++ hack/aotutil/main.go
@@ -2,25 +2,115 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
+	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
 
 func main() {
-	if err := run(); err != nil {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx := context.Background()
+	var aFlags AwsFlags
+	cmdCtx := CmdContext{
+		Logger:   logger,
+		AwsFlags: &aFlags,
+	}
+	root := cobra.Command{
+		Use:   "aotutil",
+		Short: "Utils for aws observability test framework",
+	}
+	root.PersistentFlags().StringVar(&aFlags.Region, "region", "us-west-2", "aws region name")
+	root.AddCommand(
+		ssmCmd(ctx, &cmdCtx),
+	)
+	if err := root.Execute(); err != nil {
 		log.Fatal(err)
 	}
 }
 
-func run() error {
-	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion("us-west-2"))
+type CmdContext struct {
+	Logger   *zap.Logger
+	AwsFlags *AwsFlags
+}
+
+type AwsFlags struct {
+	Region string
+}
+
+func (f *AwsFlags) MustLoadConfig(ctx context.Context, logger *zap.Logger) aws.Config {
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("us-west-2"))
 	if err != nil {
-		return fmt.Errorf("load sdk config failed: %w", err)
+		logger.Fatal("Init AWS config failed", zap.Error(err))
 	}
-	s := NewSSM(cfg, zap.NewExample())
-	return s.WaitPatch(context.Background(), "123", 30*time.Minute)
+	return cfg
+}
+
+func ssmCmd(ctx context.Context, cmdCtx *CmdContext) *cobra.Command {
+	logger := cmdCtx.Logger.With(zap.String("Command", "ssm"))
+	var (
+		ssmWrapper     *SSMWrapper
+		ssmWaitTimeout time.Duration
+	)
+	root := cobra.Command{
+		Use:   "ssm",
+		Short: "Wrapper for SSm (AWS Systems Manager)",
+		PersistentPreRun: func(cmd *cobra.Command, args []string) {
+			cfg := cmdCtx.AwsFlags.MustLoadConfig(ctx, logger)
+			ssmWrapper = NewSSM(cfg, logger)
+		},
+	}
+	root.Flags().DurationVar(&ssmWaitTimeout, "timeout", 15*time.Minute, "abort polling if timeout duration is exceeded")
+
+	oneInstanceId := func(args []string) string {
+		if len(args) == 0 {
+			logger.Fatal("Instance ID is required as position arguments")
+		}
+		if len(args) > 1 {
+			logger.Warn("Only one instance ID is supported", zap.Int("Got", len(args)))
+		}
+		return args[0]
+	}
+	// Wait Patch
+	waitPatch := cobra.Command{
+		Use:   "wait-patch",
+		Short: "Wait until patch association is completed or timeout",
+		Example: `# Wait patch on ec2 instance i-123456 for 5 minutes
+aotutil ssm wait-patch i-1234356 --timeout 5m`,
+		Run: func(cmd *cobra.Command, args []string) {
+			instance := oneInstanceId(args)
+			if err := ssmWrapper.WaitPatch(ctx, instance, ssmWaitTimeout); err != nil {
+				logger.Fatal("WaitPatch failed", zap.Error(err))
+			}
+			logger.Info("WaitPatch done")
+		},
+	}
+
+	// Wait Patch Report
+	watchPatchReport := cobra.Command{
+		Use:   "wait-patch-report",
+		Short: "Wait until a patched instance is reported",
+		Run: func(cmd *cobra.Command, args []string) {
+			instance := oneInstanceId(args)
+			if err := ssmWrapper.WaitPatchReported(ctx, instance, ssmWaitTimeout); err != nil {
+				logger.Fatal("WaitPatch failed", zap.Error(err))
+			} else {
+
+			}
+			logger.Info("WaitPatch done")
+		},
+	}
+
+	root.AddCommand(
+		&waitPatch,
+		&watchPatchReport,
+	)
+	return &root
 }
--- hack/aotutil/patch.go
+++ hack/aotutil/patch.go
@@ -27,8 +27,8 @@ type SSMWrapper struct {
 }
 
 const (
-	waitInterval           = time.Minute
-	waitPatchReportTimeout = 30 * time.Minute // this is the minimal ssm association interval
+	waitInterval                  = time.Minute
+	waitPatchReportMinimalTimeout = 30 * time.Minute // this is the minimal ssm association interval
 )
 
 func NewSSM(cfg aws.Config, logger *zap.Logger) *SSMWrapper {
@@ -40,70 +40,63 @@ func NewSSM(cfg aws.Config, logger *zap.Logger) *SSMWrapper {
 // NOTE: there is no builtin waiter implementation for checking association status.
 func (s *SSMWrapper) WaitPatch(ctx context.Context, instanceId string, timeout time.Duration) error {
 	logger := s.logger.With(zap.String("InstanceId", instanceId), zap.String("Action", "WaitPatch"))
+	logger.Info("Start waiting patch")
 	start := time.Now()
-	timer := time.After(timeout)
-	ticker := time.NewTicker(waitInterval)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			infos, err := describeInstanceAssocStatus(ctx, s.client, instanceId)
-			if err != nil {
-				return err
-			}
-			for _, assoc := range infos {
-				if aws.ToString(assoc.Name) != SSMPatchDocument {
-					continue
-				}
-				status := aws.ToString(assoc.Status)
-				switch status {
-				case "Success":
-					logger.Info("patch on instance succeeded", zap.Duration("Waited", time.Now().Sub(start)))
-					return nil
-				case "Failed":
-					return fmt.Errorf("patch on instance failed instanceId %s waited %s", instanceId, time.Now().Sub(start))
-				default:
-					logger.Info("waiting patching", zap.String("Status", status))
-				}
-			}
-		case <-timer:
-			return fmt.Errorf("wait patch timeout on instace %s after %s", instanceId, time.Now().Sub(start))
+	return Wait(waitInterval, timeout, func() (WaitAction, error) {
+		infos, err := describeInstanceAssocStatus(ctx, s.client, instanceId)
+		if err != nil {
+			return WaitDone, err
 		}
-	}
-}
-
-func (s *SSMWrapper) WaitPatchReported(ctx context.Context, instanceId string) (bool, error) {
-	logger := s.logger.With(zap.String("InstanceId", instanceId), zap.String("Action", "WaitPatchReported"))
-	start := time.Now()
-	timer := time.After(waitPatchReportTimeout)
-	ticker := time.NewTicker(waitInterval)
-	defer ticker.Stop()
-	for {
-		select {
-		case <-ticker.C:
-			infos, err := describeInstanceAssocStatus(ctx, s.client, instanceId)
-			if err != nil {
-				return false, err
-			}
-			var patchTime, reportTime time.Time
-			for _, assoc := range infos {
-				switch aws.ToString(assoc.Name) {
-				case SSMPatchDocument:
-					patchTime = aws.ToTime(assoc.ExecutionDate)
-				case SSMReportDocument:
-					reportTime = aws.ToTime(assoc.ExecutionDate)
-				}
-			}
-			logger.Info("waiting patch report", zap.Time("PatchTime", patchTime), zap.Time("ReportTime", reportTime))
-			if patchTime.IsZero() || reportTime.IsZero() || reportTime.Before(patchTime) {
+		for _, assoc := range infos {
+			if aws.ToString(assoc.Name) != SSMPatchDocument {
 				continue
 			}
-			logger.Info("patch reported")
-			return true, nil
-		case <-timer:
-			return false, fmt.Errorf("wait patch report timeout after %s", time.Now().Sub(start))
+			status := aws.ToString(assoc.Status)
+			switch status {
+			case "Success":
+				logger.Info("patch on instance succeeded",
+					zap.Time("PatchedOn", aws.ToTime(assoc.ExecutionDate)),
+					zap.Duration("Waited", time.Now().Sub(start)))
+				return WaitDone, nil
+			case "Failed":
+				return WaitDone, fmt.Errorf("patch on instance failed instanceId %s waited %s", instanceId, time.Now().Sub(start))
+			default:
+				logger.Info("waiting patching", zap.String("Status", status))
+				return WaitContinue, nil
+			}
 		}
+		return WaitDone, fmt.Errorf("patch document is not associated on the instance, requiores %s", SSMPatchDocument)
+	})
+}
+
+func (s *SSMWrapper) WaitPatchReported(ctx context.Context, instanceId string, timeout time.Duration) error {
+	logger := s.logger.With(zap.String("InstanceId", instanceId), zap.String("Action", "WaitPatchReported"))
+	logger.Info("Start waiting patch report")
+	// Force the minial wait time for patch report
+	if timeout < waitPatchReportMinimalTimeout {
+		timeout = waitPatchReportMinimalTimeout
 	}
+	return Wait(waitInterval, timeout, func() (WaitAction, error) {
+		infos, err := describeInstanceAssocStatus(ctx, s.client, instanceId)
+		if err != nil {
+			return WaitDone, err
+		}
+		var patchTime, reportTime time.Time
+		for _, assoc := range infos {
+			switch aws.ToString(assoc.Name) {
+			case SSMPatchDocument:
+				patchTime = aws.ToTime(assoc.ExecutionDate)
+			case SSMReportDocument:
+				reportTime = aws.ToTime(assoc.ExecutionDate)
+			}
+		}
+		logger.Info("waiting patch report", zap.Time("PatchTime", patchTime), zap.Time("ReportTime", reportTime))
+		if patchTime.IsZero() || reportTime.IsZero() || reportTime.Before(patchTime) {
+			return WaitContinue, nil
+		}
+		logger.Info("patch reported")
+		return WaitDone, nil
+	})
 }
 
 func describeInstanceAssocStatus(ctx context.Context, client *ssm.Client, instanceId string) ([]types.InstanceAssociationStatusInfo, error) {
