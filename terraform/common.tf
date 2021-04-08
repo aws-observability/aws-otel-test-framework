9ca1043e4136b944f14a7b8ff9d8a65697cf7f6c
Pinglei Guo <pingleig@amazon.com>
2021-04-08 13:49:15 -0700

    patch: Use aotutil for check patch and wait report

--- terraform/common.tf
+++ terraform/common.tf
@@ -71,3 +71,7 @@ variable "cortex_instance_endpoint" {
   # change to your cortex endpoint
   default = "https://aps-workspaces-gamma.us-west-2.amazonaws.com/workspaces/ws-31eb305d-3208-42d5-a7f4-32ce1191e699"
 }
+
+variable "aotutil" {
+  default = "../../cmd/aotutil/aotutil"
+}
