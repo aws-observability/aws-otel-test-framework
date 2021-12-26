1959a5ff63e7596690ed5d9e27f651a191408378
Khanh Nguyen <khanhntd@amazon.com>
2021-12-25 22:12:37 -0600

    add initial support for dynamic account

--- terraform/setup/appmesh.tf
+++ terraform/setup/appmesh.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_policy" "appmesh_k8s_iam_policy" {
-  name = "AWSAppMeshK8sControllerIAMPolicy"
+  name = "${module.common.appmesh_k8s_iam_policy}"
   path = "/"
 
   # Terraform's jsonencode function converts a
