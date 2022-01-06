de1a499623a68ed6d6822dbe749fd648e37f69e4
Jeffrey Chien <chienjef@amazon.com>
2022-01-06 11:40:47 -0500

    Reduced instance type size to t2.micro. (#466)

--- terraform/ec2/main.tf
+++ terraform/ec2/main.tf
@@ -87,7 +87,7 @@ locals {
 ## launch a sidecar instance to install data emitter and the mocked server
 resource "aws_instance" "sidecar" {
   ami                         = data.aws_ami.amazonlinux2.id
-  instance_type               = "m5.2xlarge"
+  instance_type               = "t2.micro"
   subnet_id                   = local.instance_subnet
   vpc_security_group_ids      = [module.basic_components.aoc_security_group_id]
   associate_public_ip_address = true
