93c36194aebaf359c7291c98884cab5f2354853b
bryan-aguilar <46550959+bryan-aguilar@users.noreply.github.com>
2022-02-18 10:03:27 -0800

    Remove ADOT Operator namespace check during helm deployment (#563)
    * Specify operator helm chart version
    
    * terraform fmt
    
    * Remove namespace check in helm release
    
    * Remove version requirement
    
    * Remove stale comments

--- terraform/eks/adot-operator/adot_operator.tf
+++ terraform/eks/adot-operator/adot_operator.tf
@@ -34,6 +34,6 @@ resource "helm_release" "adot-operator" {
   ]
 
   provisioner "local-exec" {
-    command = "kubectl wait --kubeconfig=${var.kubeconfig} --timeout=5m --for=condition=available deployment opentelemetry-operator-controller-manager -n opentelemetry-operator-system"
+    command = "kubectl wait --kubeconfig=${var.kubeconfig} --timeout=5m --for=condition=available deployment opentelemetry-operator-controller-manager"
   }
 }
