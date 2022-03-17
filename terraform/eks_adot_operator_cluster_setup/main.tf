137819d92d7964ed37b526191ec60e6d01ee66c2
Kausik-a <kausika@amazon.com>
2022-03-17 16:21:09 -0700

    Saving terraform state file to a s3 bucket

--- terraform/eks_adot_operator_cluster_setup/main.tf
+++ terraform/eks_adot_operator_cluster_setup/main.tf
@@ -20,6 +20,13 @@ terraform {
       version = ">= 1.7.0"
     }
   }
+
+  ## comment out the backend block if you want the terraform state file to be locally saved on the computer (or) change the name of the s3 bucket if you are testing out on your personal AWS account
+  backend "s3"{
+    bucket = "adot-cluster-terraform-statefile"
+    key = "global/s3/terraform.tfstate"
+    region = "us-west-2"
+  }
 }
 
 module "common" {
