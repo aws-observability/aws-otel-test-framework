40b7800c2100d42ea03aca48523b31fc511bd114
Kausik <kausika@amazon.com>
2022-04-05 13:33:45 -0700

    following the terraform styling conventions

--- terraform/eks_arm64_cluster_setup/main.tf
+++ terraform/eks_arm64_cluster_setup/main.tf
@@ -32,28 +32,35 @@ provider "aws" {
 }
 
 module "vpc" {
-  source = "terraform-aws-modules/vpc/aws"
+  source  = "terraform-aws-modules/vpc/aws"
   version = "3.11.5"
+
   name = "${var.eks_cluster_name}-vpc"
   cidr = "10.0.0.0/16"
+
   azs             = ["${var.region}a", "${var.region}b", "${var.region}c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
   enable_nat_gateway = true
   enable_vpn_gateway = true
   single_nat_gateway = true
 }
 
 module "eks" {
-  source = "terraform-aws-modules/eks/aws"
+  source  = "terraform-aws-modules/eks/aws"
   version = "18.6.0"
+
   cluster_version = "1.21"
   cluster_name    = var.eks_cluster_name
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.public_subnets
   enable_irsa     = true
+
   cluster_endpoint_public_access = true
+
   cluster_enabled_log_types = ["api", "audit", "authenticator", "controllerManager", "scheduler"]
+
   eks_managed_node_groups = {
     arm64_node_group = {
       ami_type     = "AL2_ARM_64"
@@ -63,6 +70,7 @@ module "eks" {
 
       instance_types = ["m6g.medium"]
       capacity_type  = "SPOT"
+
       iam_role_additional_policies = [
         "arn:aws:iam::aws:policy/AmazonPrometheusRemoteWriteAccess",
         "arn:aws:iam::aws:policy/AWSXrayWriteOnlyAccess",
