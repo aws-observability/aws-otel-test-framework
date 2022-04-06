ec5db44bfaf9c78599540c1158eb3d516ee92a61
Bryan Aguilar <bryaag@amazon.com>
2022-04-06 10:12:07 -0700

    Update provider version constraint

--- terraform/setup/setup.tf
+++ terraform/setup/setup.tf
@@ -19,6 +19,14 @@
 module "common" {
   source = "../common"
 }
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.8.0"
+    }
+  }
+}
 
 provider "aws" {
   region = var.region
