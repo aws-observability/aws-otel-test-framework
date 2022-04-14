5d2788a1c304e03ebcf64029fa47fb9ed193991b
Bryan Aguilar <bryaag@amazon.com>
2022-04-14 10:53:23 -0700

    Initial batchgenerator commit

---
+++ tools/batchTestGenerator/main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/aws-observability/aws-otel-test-frameowrk/batchTestGenerator/cmd"
+
+func main() {
+	cmd.Execute()
+}
