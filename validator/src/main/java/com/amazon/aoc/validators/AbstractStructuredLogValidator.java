d874e67762dde637cdd1c1200f03ca1ae7607003
bryan-aguilar <46550959+bryan-aguilar@users.noreply.github.com>
2022-06-08 13:45:57 -0700

    Cherry-pick logging changes into release/v0.17.x branch (#703)
    * Add terraform cleanup (#675)
    
    * Add logging to structured logs validator (#677)
    
    * Add logging around log query
    
    * add loging around json validation
    
    * Print entire processing report instead of messages (#679)
    
    * print entire report
    
    * add newline
    
    * Add visbility into query (#681)
    
    * add visbility into query
    
    * fix formatting
    
    * Log check result fails and improve failed schema output (#683)
    
    * add logging
    
    * Add more logging, failed results
    
    * Update validator/src/main/java/com/amazon/aoc/validators/AbstractStructuredLogValidator.java
    
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>
    
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>
    
    Co-authored-by: Anthony Mirabella <a9@aneurysm9.com>

--- validator/src/main/java/com/amazon/aoc/validators/AbstractStructuredLogValidator.java
+++ validator/src/main/java/com/amazon/aoc/validators/AbstractStructuredLogValidator.java
@@ -38,11 +38,7 @@ import lombok.extern.log4j.Log4j2;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 @Log4j2
 public abstract class AbstractStructuredLogValidator implements IValidator {
@@ -91,7 +87,6 @@ public abstract class AbstractStructuredLogValidator implements IValidator {
     RetryHelper.retry(getMaxRetryCount(), CHECK_INTERVAL_IN_MILLI, true, () -> {
       Instant startTime = Instant.now().minusSeconds(CHECK_DURATION_IN_SECONDS)
               .truncatedTo(ChronoUnit.MINUTES);
-
       fetchAndValidateLogs(startTime);
       checkResult();
     });
@@ -124,18 +119,23 @@ public abstract class AbstractStructuredLogValidator implements IValidator {
 
   private void checkResult() throws BaseException {
     if (schemasToValidate.isEmpty() || schemasToValidate.keySet().equals(validatedSchema)) {
+      log.info(String.format("[StructuredLogValidator] log structure validation successful"
+              + "isEmpty: %b isEqual: %b",
+              schemasToValidate.isEmpty(), schemasToValidate.keySet().equals(validatedSchema)));
       return;
     }
-    String[] failedTargets = new String[schemasToValidate.size()];
-    int i = 0;
+    ArrayList<String> failedTargets = new ArrayList<>();
     for (String key : schemasToValidate.keySet()) {
-      failedTargets[i] = key;
-      i++;
+      if (!validatedSchema.contains(key)) {
+        failedTargets.add(key);
+      }
     }
+    log.info(String.format("[StructuredLogValidator] log structure validation failed for %s",
+            failedTargets));
     throw new BaseException(
             ExceptionCode.LOG_FORMAT_NOT_MATCHED,
             String.format("[StructuredLogValidator] log structure validation failed for %s",
-                    StringUtils.join(",", failedTargets)));
+                    failedTargets));
   }
 
   protected void validateJsonSchema(String logEventMsg) throws Exception {
@@ -150,6 +150,11 @@ public abstract class AbstractStructuredLogValidator implements IValidator {
               JsonLoader.fromString(logEventNode.toString()));
       if (report.isSuccess()) {
         validatedSchema.add(key);
+      } else {
+        // This will probably generate a lot of extra logs
+        // may want to log this to a different level in the future.
+        log.info("[StructuredLogValidator] failed to validate schema \n");
+        log.info(report.toString() + "\n");
       }
     }
   }
