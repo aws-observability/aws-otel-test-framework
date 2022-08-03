6b67308b20ce0063c651717772e03f622d0de752
Eric Hsueh <39718333+erichsueh3@users.noreply.github.com>
2022-08-03 16:29:53 -0700

    changed method of setting op cluster variable + other tf vars (#784)

--- terraform/executeTerraformTest.sh
+++ terraform/executeTerraformTest.sh
@@ -37,7 +37,7 @@ service="$1"
 export AWS_REGION=us-west-2
 case "$service" in
     "EC2") TEST_FOLDER="./ec2/";
-        export TF_VAR_testing_ami=$3;
+        opts+=" -var=testing_ami=$3";
     ;;
     "EKS") TEST_FOLDER="./eks/";
     ;;
@@ -45,17 +45,18 @@ case "$service" in
         arm_64_region=$(echo $3 | cut -d \| -f 1);
         arm_64_clustername=$(echo $3 | cut -d \| -f 2);
         arm_64_amp=$(echo $3 | cut -d \| -f 3);
-        export AWS_REGION=${arm_64_region}
-        export TF_VAR_region=${arm_64_region}
-        export TF_VAR_cortex_instance_endpoint=${arm_64_amp}
-        export TF_VAR_eks_cluster_name=${arm_64_clustername}
+        export AWS_REGION=${arm_64_region};
+        opts+=" -var=region=${arm_64_region}";
+        opts+=" -var=cortex_instance_endpoint=${arm_64_amp}";
+        opts+=" -var=eks_cluster_name=${arm_64_clustername}";
     ;;
     "EKS_FARGATE") TEST_FOLDER="./eks/";
     ;;
     "EKS_ADOT_OPERATOR") TEST_FOLDER="./eks/";
+        opts+=" -var=eks_cluster_name=adot-op-cluster";
     ;;
     "ECS") TEST_FOLDER="./ecs/";
-        export TF_VAR_ecs_launch_type=$3;
+        opts+=" -var=ecs_launch_type=$3";
     ;;
     *)
     echo "service ${service} is not valid";
