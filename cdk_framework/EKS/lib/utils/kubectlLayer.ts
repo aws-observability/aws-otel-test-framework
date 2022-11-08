3a2589aa980c230b618f1a56f649911de2d31581
Bryan Aguilar <bryaag@amazon.com>
2022-11-07 17:37:21 -0800

    Use v21 layer or throw error

--- cdk_framework/EKS/lib/utils/kubectlLayer.ts
+++ cdk_framework/EKS/lib/utils/kubectlLayer.ts
@@ -1,8 +1,8 @@
 import { KubectlV22Layer } from "@aws-cdk/lambda-layer-kubectl-v22";
 import { KubectlV23Layer } from "@aws-cdk/lambda-layer-kubectl-v23";
+import { KubectlLayer } from "@aws-cdk/lambda-layer-kubectl-v21";
 import { aws_eks } from "aws-cdk-lib";
 import { ILayerVersion } from "aws-cdk-lib/aws-lambda";
-import { KubectlLayer } from "aws-cdk-lib/lambda-layer-kubectl";
 import { Construct } from "constructs/lib/construct";
 
 export function GetLayer(scope: Construct, k8sVersion : aws_eks.KubernetesVersion):  ILayerVersion{
@@ -11,7 +11,9 @@ export function GetLayer(scope: Construct, k8sVersion : aws_eks.KubernetesVersio
         return new KubectlV22Layer(scope, 'v22Layer');
       case aws_eks.KubernetesVersion.V1_23:
         return  new KubectlV23Layer(scope,'v23Layer');
+      case aws_eks.KubernetesVersion.V1_21:
+        return new KubectlLayer(scope, 'v21Layer');
       default:
-        return new KubectlLayer(scope, 'v20Layer');
+        throw new Error(`invalid kubernetes version: ${k8sVersion.version}`)
     }
   }
