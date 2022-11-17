c2c3fb918f408a3148f6ab3b00db5060a47580b9
Bryan Aguilar <bryaag@amazon.com>
2022-11-16 16:51:03 -0800

    Move validation to eks subdir

--- cdk_infra/lib/utils/eks/validate-interface-schema.ts
+++ cdk_infra/lib/utils/eks/validate-interface-schema.ts
@@ -1,5 +1,5 @@
-import { ClusterInterface } from '../interfaces/eks/cluster-interface';
-import { ec2ClusterInterface } from '../interfaces/eks/ec2cluster-interface';
+import { ClusterInterface } from '../../interfaces/eks/cluster-interface';
+import { ec2ClusterInterface } from '../../interfaces/eks/ec2cluster-interface';
 
 const validateSchema = require('yaml-schema-validator');
 
