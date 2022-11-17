d5ad082b99a01068ef0d3fba9e2a3b1dfe98d0b2
Bryan Aguilar <bryaag@amazon.com>
2022-11-17 10:50:41 -0800

    Add eks v1.24 support

--- cdk_infra/lib/utils/eks/kubectlLayer.ts
+++ cdk_infra/lib/utils/eks/kubectlLayer.ts
@@ -4,18 +4,21 @@ import { KubectlLayer } from '@aws-cdk/lambda-layer-kubectl-v21';
 import { aws_eks } from 'aws-cdk-lib';
 import { ILayerVersion } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs/lib/construct';
+import { KubectlV24Layer } from '@aws-cdk/lambda-layer-kubectl-v24';
 
 export function GetLayer(
   scope: Construct,
   k8sVersion: aws_eks.KubernetesVersion
 ): ILayerVersion {
   switch (k8sVersion.version) {
+    case '1.21':
+      return new KubectlLayer(scope, 'v21Layer');
     case '1.22':
       return new KubectlV22Layer(scope, 'v22Layer');
     case '1.23':
       return new KubectlV23Layer(scope, 'v23Layer');
-    case '1.21':
-      return new KubectlLayer(scope, 'v21Layer');
+    case '1.24':
+      return new KubectlV24Layer(scope, 'v24Layer')
     default:
       throw new Error(`invalid kubernetes version: ${k8sVersion.version}`);
   }
