c440f4825f07c1154b8fff9fb88f7e2e53e5bb11
Huy Vo <hvoam@amazon.com>
2023-01-11 12:42:25 -0800

    Add launch template for nodegroup to require IMDSv2

--- cdk_infra/lib/stacks/eks/ec2-cluster-stack.ts
+++ cdk_infra/lib/stacks/eks/ec2-cluster-stack.ts
@@ -1,7 +1,7 @@
 import { Stack, StackProps, aws_eks as eks, aws_ec2 as ec2 } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { Vpc } from 'aws-cdk-lib/aws-ec2';
-import { KubernetesVersion, Nodegroup } from 'aws-cdk-lib/aws-eks';
+import { KubernetesVersion, Nodegroup, LaunchTemplateSpec } from 'aws-cdk-lib/aws-eks';
 import { ManagedPolicy } from 'aws-cdk-lib/aws-iam';
 import { GetLayer } from '../../utils/eks/kubectlLayer';
 
@@ -28,11 +28,22 @@ export class EC2Stack extends Stack {
       clusterLogging: logging,
       kubectlLayer: GetLayer(this, props.version)
     });
+    const lt = new ec2.LaunchTemplate(this, `${props.name}-launch-template`, {
+      requireImdsv2: true,
+      httpEndpoint: true,
+      httpPutResponseHopLimit: 2,
+      httpTokens: ec2.LaunchTemplateHttpTokens.REQUIRED,
+      
+    })
     const clusterNodeGroup = new Nodegroup(this, `${props.name}-managed-ng`, {
       instanceTypes: [new ec2.InstanceType(instance_type)],
       cluster: this.cluster,
       minSize: 2,
-      subnets: { subnetType: ec2.SubnetType.PUBLIC }
+      subnets: { subnetType: ec2.SubnetType.PUBLIC },
+      launchTemplateSpec: {
+        id: lt.launchTemplateId as string,
+        version: lt.latestVersionNumber,
+      }
     });
     clusterNodeGroup.role.addManagedPolicy(
       ManagedPolicy.fromAwsManagedPolicyName('AmazonSSMManagedInstanceCore')
