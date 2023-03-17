4fff0a5077f15e601b8f73a8524b5e59ec7fe106
Pavan Sai Vasireddy (Vasi) <pvasir@amazon.com>
2023-03-17 02:06:09 -0700

    Use Appropriate names and specific validator

--- validator/src/main/java/com/amazon/aoc/validators/ContainerInsightStructuredLogValidatorDocker.java
+++ validator/src/main/java/com/amazon/aoc/validators/ContainerInsightStructuredLogValidatorDocker.java
@@ -16,7 +16,7 @@ import java.util.Arrays;
 import java.util.List;
 
 @Log4j2
-public class ContainerInsightStructuredLogValidator
+public class ContainerInsightStructuredLogValidatorDocker
         extends AbstractStructuredLogValidator {
 
   //expected template Array for docker runtime
@@ -34,36 +34,15 @@ public class ContainerInsightStructuredLogValidator
           "PodNet"
   );
 
-  //expected template Array for containerd runtime
-  private static final List<String> LOG_TYPE_CONTAINERD = Arrays.asList(
-          "Cluster",
-          "ClusterNamespace",
-          "ClusterService",
-          "Container",
-          "Node",
-          "NodeDiskIO",
-          "NodeFS",
-          "NodeNet",
-          "Pod",
-          "PodNet"
-  );
-
   private static final int MAX_RETRY_COUNT = 15;
   private static final int QUERY_LIMIT = 100;
-  private static List<String> LOG_TYPE_TO_VALIDATE;
 
   @Override
   void init(Context context, FileConfig expectedDataTemplate) throws Exception {
     logGroupName = String.format("/aws/containerinsights/%s/performance",
             context.getCloudWatchContext().getClusterName());
     MustacheHelper mustacheHelper = new MustacheHelper();
-    if (context.getTestcase().contains("containerinsight_eks")) {
-      LOG_TYPE_TO_VALIDATE = LOG_TYPE_DOCKER;
-    } else {
-      LOG_TYPE_TO_VALIDATE = LOG_TYPE_CONTAINERD;
-    }
-
-    for (String logType : LOG_TYPE_TO_VALIDATE) {
+    for (String logType : LOG_TYPE_DOCKER) {
       FileConfig fileConfig = new LocalPathExpectedTemplate(FilenameUtils.concat(
               expectedDataTemplate.getPath().toString(),
               logType + ".json"));
