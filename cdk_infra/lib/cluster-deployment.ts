40970213d2cefbe555ef210bf1fe20ea6b7270ee
Bryan Aguilar <bryaag@amazon.com>
2023-04-18 08:19:07 -0700

    make pretty

--- cdk_infra/lib/cluster-deployment.ts
+++ cdk_infra/lib/cluster-deployment.ts
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import {aws_eks as eks, StackProps} from 'aws-cdk-lib';
-import {readFileSync} from 'fs';
-import {EC2Stack} from './stacks/eks/ec2-cluster-stack';
-import {FargateStack} from './stacks/eks/fargate-cluster-stack';
-import {validateFileSchema} from './utils/eks/validate-config-schema';
-import {ClusterInterface} from './interfaces/eks/cluster-interface';
-import {ec2ClusterInterface} from './interfaces/eks/ec2cluster-interface';
-import {validateInterface} from './utils/eks/validate-interface-schema';
-import {ClusterAuth} from './constructs/eks/clusterAuthConstruct';
-import {HelmChart, NodegroupAmiType, OpenIdConnectProvider} from 'aws-cdk-lib/aws-eks';
-import {InstanceType, Vpc} from 'aws-cdk-lib/aws-ec2';
+import { aws_eks as eks, StackProps } from 'aws-cdk-lib';
+import { readFileSync } from 'fs';
+import { EC2Stack } from './stacks/eks/ec2-cluster-stack';
+import { FargateStack } from './stacks/eks/fargate-cluster-stack';
+import { validateFileSchema } from './utils/eks/validate-config-schema';
+import { ClusterInterface } from './interfaces/eks/cluster-interface';
+import { ec2ClusterInterface } from './interfaces/eks/ec2cluster-interface';
+import { validateInterface } from './utils/eks/validate-interface-schema';
+import { ClusterAuth } from './constructs/eks/clusterAuthConstruct';
+import {
+  HelmChart,
+  NodegroupAmiType,
+  OpenIdConnectProvider
+} from 'aws-cdk-lib/aws-eks';
+import { InstanceType, Vpc } from 'aws-cdk-lib/aws-ec2';
 
 const yaml = require('js-yaml');
 
@@ -56,8 +60,12 @@ export function deployClusters(
         name: ec2Cluster.name,
         vpc: vpc,
         version: versionKubernetes,
-        instanceTypes: [new InstanceType(ec2Cluster.instance_type.toLowerCase())],
-        amiType: ec2Cluster.name.match('\-arm64\-') ? NodegroupAmiType.AL2_ARM_64 : NodegroupAmiType.AL2_X86_64,
+        instanceTypes: [
+          new InstanceType(ec2Cluster.instance_type.toLowerCase())
+        ],
+        amiType: ec2Cluster.name.match('-arm64-')
+          ? NodegroupAmiType.AL2_ARM_64
+          : NodegroupAmiType.AL2_X86_64,
         env: envInput
       });
     } else {
