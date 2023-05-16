45d0e01e008af48e3697cc4dbc066ce62ca414fd
Bryan Aguilar <bryaag@amazon.com>
2023-05-16 15:37:44 -0700

    Add new kubectl layers

--- cdk_infra/lib/utils/eks/kubectlLayer.ts
+++ cdk_infra/lib/utils/eks/kubectlLayer.ts
@@ -5,6 +5,8 @@ import { aws_eks } from 'aws-cdk-lib';
 import { ILayerVersion } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs/lib/construct';
 import { KubectlV24Layer } from '@aws-cdk/lambda-layer-kubectl-v24';
+import { KubectlV25Layer } from '@aws-cdk/lambda-layer-kubectl-v25';
+import { KubectlV26Layer } from '@aws-cdk/lambda-layer-kubectl-v26';
 
 export function GetLayer(
   scope: Construct,
@@ -19,6 +21,10 @@ export function GetLayer(
       return new KubectlV23Layer(scope, 'v23Layer');
     case '1.24':
       return new KubectlV24Layer(scope, 'v24Layer');
+    case '1.25':
+      return new KubectlV25Layer(scope, 'v25Layer');
+    case '1.26':
+      return new KubectlV26Layer(scope, 'v26Layer');
     default:
       throw new Error(`invalid kubernetes version: ${k8sVersion.version}`);
   }
