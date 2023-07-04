6d6644b52f9dc684efc5f40cadee213b30355f11
Paurush Garg <paurushg@amazon.com>
2023-07-03 21:59:23 -0700

    Using Kubectl_manifest instead of null_resource

--- terraform/eks/otlp.tf
+++ terraform/eks/otlp.tf
@@ -107,7 +107,7 @@ module "aoc_oltp" {
   is_adot_operator                = replace(var.testcase, "_adot_operator", "") != var.testcase
   is_inject_auto_instrumentation  = var.is_inject_auto_instrumentation
 
-  depends_on = [module.iam_assumable_role_sample_app, null_resource.java_auto_instrumentation_adot_operator]
+  depends_on = [module.iam_assumable_role_sample_app, kubectl_manifest.java_auto_instrumentation_deployment]
 }
 
 locals {
@@ -293,27 +293,22 @@ resource "null_resource" "aoc_deployment_adot_operator" {
   depends_on = [module.adot_operator]
 }
 
-resource "local_file" "java_auto_instrumentation_deployment" {
+resource "kubectl_manifest" "java_auto_instrumentation_deployment" {
   count = var.aoc_base_scenario == "oltp" && replace(var.testcase, "_adot_operator", "") != var.testcase && var.is_inject_auto_instrumentation ? 1 : 0
 
-  filename = "java_auto_instrumentation.yaml"
-
-  content = templatefile("./adot-operator/java_auto_instrumentation.tpl",
-    {
-      AOC_NAMESPACE                        = var.deployment_type == "fargate" ? kubernetes_namespace.aoc_fargate_ns.metadata[0].name : kubernetes_namespace.aoc_ns.metadata[0].name
-      JAVA_AUTO_INSTRUMENTATION_REPOSITORY = var.java_auto_instrumentation_repository
-      JAVA_AUTO_INSTRUMENTATION_TAG        = var.java_auto_instrumentation_tag
-  })
-
-  depends_on = [module.adot_operator]
-}
-
-resource "null_resource" "java_auto_instrumentation_adot_operator" {
-  count = var.aoc_base_scenario == "oltp" && replace(var.testcase, "_adot_operator", "") != var.testcase && var.is_inject_auto_instrumentation ? 1 : 0
-
-  provisioner "local-exec" {
-    command = "kubectl apply --kubeconfig=${local_file.kubeconfig.filename} -f ${local_file.java_auto_instrumentation_deployment.0.filename}"
-  }
+  yaml_body = <<-EOF
+    apiVersion: opentelemetry.io/v1alpha1
+    kind: Instrumentation
+    metadata:
+      name: my-instrumentation
+      namespace: ${var.deployment_type == "fargate" ? kubernetes_namespace.aoc_fargate_ns.metadata[0].name : kubernetes_namespace.aoc_ns.metadata[0].name}
+    spec:
+      sampler:
+        type: parentbased_traceidratio
+        argument: "1.0"
+      java:
+        image: ${var.java_auto_instrumentation_repository}:${var.java_auto_instrumentation_tag}
+    EOF
 
   depends_on = [module.adot_operator]
 }
