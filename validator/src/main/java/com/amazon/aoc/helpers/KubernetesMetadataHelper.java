54cdb149ad5e22d4ff8beffda89c0ac51ddc903e
Bryan Aguilar <bryaag@amazon.com>
2023-07-17 14:28:17 -0700

    Refactor k8s api and sample app query into separate class

---
+++ validator/src/main/java/com/amazon/aoc/helpers/KubernetesMetadataHelper.java
@@ -0,0 +1,37 @@
+package com.amazon.aoc.helpers;
+
+import io.kubernetes.client.extended.kubectl.Kubectl;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.models.V1Pod;
+import io.kubernetes.client.util.Config;
+import java.io.IOException;
+import java.util.List;
+import java.util.Objects;
+
+/*
+ * By default, creates a kubernetes client using the provided kubernetes config file path
+ */
+public class KubernetesMetadataHelper {
+  private final String kubeConfigFilePath;
+
+  public KubernetesMetadataHelper(String kubecfgfp) throws IOException {
+    this.kubeConfigFilePath = kubecfgfp;
+    Configuration.setDefaultApiClient(buildK8sAPIClient());
+  }
+
+  private ApiClient buildK8sAPIClient() throws IOException {
+    return Config.fromConfig(this.kubeConfigFilePath);
+  }
+
+  public V1Pod getSampleAppPod(String deploymentName, String namespace) throws Exception {
+    List<V1Pod> pods = Kubectl.get(V1Pod.class).namespace(namespace).execute();
+    for (V1Pod pod : pods) {
+      if (Objects.requireNonNull(Objects.requireNonNull(pod.getMetadata()).getName())
+          .startsWith(deploymentName)) {
+        return pod;
+      }
+    }
+    return null;
+  }
+}
--- validator/src/main/java/com/amazon/aoc/models/kubernetes/KubernetesContextFactory.java
+++ validator/src/main/java/com/amazon/aoc/models/kubernetes/KubernetesContextFactory.java
@@ -1,11 +1,7 @@
 package com.amazon.aoc.models.kubernetes;
 
-import io.kubernetes.client.extended.kubectl.Kubectl;
-import io.kubernetes.client.openapi.ApiClient;
-import io.kubernetes.client.openapi.Configuration;
+import com.amazon.aoc.helpers.KubernetesMetadataHelper;
 import io.kubernetes.client.openapi.models.V1Pod;
-import io.kubernetes.client.util.Config;
-import java.util.List;
 import java.util.Objects;
 
 public class KubernetesContextFactory {
@@ -14,46 +10,50 @@ public class KubernetesContextFactory {
 
   private final String namespace;
 
+  private final KubernetesMetadataHelper kubernetesMetadataHelper;
+
   public KubernetesContextFactory(
-      String kubeConfigFilePath, String deploymentName, String namespace) {
+      String kubeConfigFilePath, String deploymentName, String namespace) throws Exception {
     this.kubeConfigFilePath = kubeConfigFilePath;
     this.deploymentName = deploymentName;
     this.namespace = namespace;
+    this.kubernetesMetadataHelper = new KubernetesMetadataHelper(this.kubeConfigFilePath);
+  }
+
+  public KubernetesContextFactory(
+      String kubeConfigFilePath,
+      String deploymentName,
+      String namespace,
+      KubernetesMetadataHelper kubernetesMetadataHelper) {
+    this.kubeConfigFilePath = kubeConfigFilePath;
+    this.deploymentName = deploymentName;
+    this.namespace = namespace;
+    this.kubernetesMetadataHelper = kubernetesMetadataHelper;
   }
 
   public KubernetesContext create() throws Exception {
     KubernetesContext kubernetesContext =
         new KubernetesContext(this.deploymentName, this.namespace);
-    ApiClient client = Config.fromConfig(this.kubeConfigFilePath);
-    Configuration.setDefaultApiClient(client);
 
-    // get all pods in namespace
-    List<V1Pod> pods = Kubectl.get(V1Pod.class).namespace(this.namespace).execute();
+    V1Pod pod = kubernetesMetadataHelper.getSampleAppPod(this.deploymentName, this.namespace);
+    if (pod != null) {
+      kubernetesContext.setNamespace(Objects.requireNonNull(pod.getMetadata()).getNamespace());
+      kubernetesContext.setPodName(pod.getMetadata().getName());
+      kubernetesContext.setPodUid(pod.getMetadata().getUid());
+      kubernetesContext.setNodeName(Objects.requireNonNull(pod.getSpec()).getNodeName());
 
-    // Go through pods until we find the first one starting with "sample-app-".
-    // Set expected values in kubernetesContext once found then exit.
-    for (V1Pod pod : pods) {
-      if (Objects.requireNonNull(Objects.requireNonNull(pod.getMetadata()).getName())
-          .startsWith(this.deploymentName)) {
-        kubernetesContext.setNamespace(pod.getMetadata().getNamespace());
-        kubernetesContext.setPodName(pod.getMetadata().getName());
-        kubernetesContext.setPodUid(pod.getMetadata().getUid());
-        kubernetesContext.setNodeName(Objects.requireNonNull(pod.getSpec()).getNodeName());
+      /*
+      Currently waiting for RF3339 format to be used before asserting against creation timestamps.
+      https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/24016
+        String creationTimestamp =
+          pod.getMetadata()
+              .getCreationTimestamp()
+              .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss xx 'UTC'"));
 
-        /*
-        Currently waiting for RF3339 format to be used before asserting against creation timestamps.
-        https://github.com/open-telemetry/opentelemetry-collector-contrib/pull/24016
-          String creationTimestamp =
-            pod.getMetadata()
-                .getCreationTimestamp()
-                .format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss xx 'UTC'"));
-
-        context.getKubernetesContext().setCreationTimeStamp(creationTimestamp);
-         */
-
-        break;
-      }
+      context.getKubernetesContext().setCreationTimeStamp(creationTimestamp);
+       */
     }
+
     return kubernetesContext;
   }
 }
