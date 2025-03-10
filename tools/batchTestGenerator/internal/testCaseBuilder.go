aeb7f803742432634889ac2fcb6d5d234c69dd65
Vasi Vasireddy <pvasir@amazon.com>
2025-03-10 14:51:44 -0700

    edit ecr emi's

--- tools/batchTestGenerator/internal/testCaseBuilder.go
+++ tools/batchTestGenerator/internal/testCaseBuilder.go
@@ -6,26 +6,8 @@ import (
 )
 
 var ec2AMIs = []string{
-	"ubuntu18",
-	"arm_ubuntu18",
-	"ubuntu20",
-	"arm_ubuntu20",
-	"ubuntu22",
-	"arm_ubuntu22",
-	"debian11",
-	"arm_debian11",
-	"debian10",
-	"arm_debian10",
-	"amazonlinux2",
-	"arm_amazonlinux2",
-	"amazonlinux3",
-	"arm_amazonlinux3",
 	"windows2022",
 	"windows2019",
-	"suse15",
-	"suse12",
-	"redhat8",
-	"arm_redhat8",
 }
 
 type TestCaseInfo struct {
