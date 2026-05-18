19482a23e5344c7e3961be37ed2dd590051b13ad
Travis Stark <tjstark@amazon.com>
2026-05-18 15:56:15 -0400

    fix: revert subnet filter to original 3 (ALB needs 1 per AZ)

--- terraform/basic_components/main.tf
+++ terraform/basic_components/main.tf
@@ -71,11 +71,8 @@ data "aws_subnets" "aoc_public_subnet_ids" {
     name = "tag:Name"
     values = [
       "${module.common.aoc_vpc_name}-public-${var.region}a",
-      "${module.common.aoc_vpc_name}-public-${var.region}a-*",
       "${module.common.aoc_vpc_name}-public-${var.region}b",
-      "${module.common.aoc_vpc_name}-public-${var.region}b-*",
       "${module.common.aoc_vpc_name}-public-${var.region}c",
-      "${module.common.aoc_vpc_name}-public-${var.region}c-*",
     ]
   }
 }
